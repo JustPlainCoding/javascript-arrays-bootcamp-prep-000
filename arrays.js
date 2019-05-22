e2cf4e8898a70bb88ebad737dfa229a6f0329d34
Eric J Wilhite <ericjwilhite@gmail.com>
2019-05-22 22:43:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,10 +2,22 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, elementToAdd) {
   var result = [elementToAdd,...array];
-  return result
+  return result;
 }
 
 function destructivelyAddElementToTheBeginningOfArray(array, elementToAdd) {
-  return array.unshift(elementToAdd)
+  return array.unshift(elementToAdd);
 }
 
+function addElementToEndOfArray(array, elementToAdd) {
+  var result = [...array, elementToAdd];
+  return result;
+}
+
+function destructivelyAddElementToTheEndOfArray(array, elementToAdd) {
+  return array.push(elementToAdd);
+}
+
+function accesElementInArray(array, index) {
+  return array[index];
+}
